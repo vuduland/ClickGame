7a25a096554b17bc7f1d5d2865d3cbeec13d989f
Victor <53414263+vuduland@users.noreply.github.com>
2020-02-18 00:26:14 -0600

    initial commit with cleaned up code, css, and one component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Clicky Game</h1>
+        <h3>Don't Click Anything Twice!!</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
